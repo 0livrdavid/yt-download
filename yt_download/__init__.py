3681b1b2d0e0d3d1eb7122190d8a8dbf23715e81
David Oliveira <davidoliveira@MacBook-Pro-de-David.local>
2026-04-18 15:01:37 -0300

    Bump version to 1.1.0

--- yt_download/__init__.py
+++ yt_download/__init__.py
@@ -3,5 +3,5 @@ YouTube Downloader
 A simple CLI tool to download YouTube videos and playlists in multiple audio formats.
 """
 
-__version__ = "1.0.2"
+__version__ = "1.1.0"
 __author__ = "David Oliveira"
